b2e4ae2e3e960414a486e4f89e9be60c520d9651
Timothy Lui <timm@Timothys-MacBook-Air.local>
2019-03-15 20:30:13 -0400

    finished q2 of java part

--- Assignment2.java
+++ Assignment2.java
@@ -11,7 +11,7 @@ import java.util.List;
 //import java.util.HashSet;
 public class Assignment2 extends JDBCSubmission {
 
-    private Connection connection;
+    public Connection connection;
 
     public Assignment2() throws ClassNotFoundException {
         Class.forName("org.postgresql.Driver");
@@ -19,7 +19,7 @@ public class Assignment2 extends JDBCSubmission {
 
     public static void main(String[] args) {
         // You can put testing code in here. It will not affect our autotester.
-        System.out.println("Hello");
+        // System.out.println("Hello");
     }
 
     @Override
@@ -46,39 +46,37 @@ public class Assignment2 extends JDBCSubmission {
 
     @Override
     public ElectionCabinetResult electionSequence(String countryName) {
-        Statement stmt = null;
-        String query =  "CREATE VIEW electionsAndCabinetsData AS" +
-                        "SELECT e.id as e_id, e.e_type as type, e.e_date as election_year," +
-                                "cab.id as cab_id, cab.start_date as cab_year," +
-                                "e.previous_parliament_election_id as prev_p_eid," +
-                                "e.previous_ep_election_id as prev_ep_eid" +
-                        "FROM country c" +
-                            "INNER JOIN election e ON c.id = e.country_id" +
-                            "INNER JOIN cabinet cab ON e.id = cab.election_id" +
-                        "WHERE c.name = " + countryName +
-                        "ORDER BY election_year DESC" +
+        String query =  "CREATE VIEW electionsAndCabinetsData AS " +
+                        "SELECT e.id as e_id, e.e_type as type, e.e_date as election_year, " +
+                                "cab.id as cab_id, cab.start_date as cab_year, " +
+                                "e.previous_parliament_election_id as prev_p_eid, " +
+                                "e.previous_ep_election_id as prev_ep_eid " +
+                        "FROM country c " +
+                            "INNER JOIN election e ON c.id = e.country_id " +
+                            "INNER JOIN cabinet cab ON e.id = cab.election_id " +
+                        "WHERE c.name = " + countryName + " " +
+                        "ORDER BY election_year DESC " +
                         "" +
-                        "SELECT DISTINCT e1.e_id, e1.cab_id" +
-                        "FROM electionsAndCabinetsData e1" +
-                            "INNER JOIN electionsAndCabinetsData e2 ON" +
-                                "(CASE WHEN e1.type = 'Parliamentary election' THEN e1.prev_p_eid" +
-                                    "WHEN e1.type = 'European Parliament' THEN e1.prev_ep_eid" +
-                                "END) = e2.e_id" +
-                        "WHERE e1.type = e2.type AND e2.cab_year > e2.election_year" +
+                        "SELECT e1.e_id, e1.cab_id " +
+                        "FROM electionsAndCabinetsData e1 " +
+                            "INNER JOIN electionsAndCabinetsData e2 ON " +
+                                "(CASE WHEN e1.type = 'Parliamentary election' THEN e1.prev_p_eid " +
+                                    "WHEN e1.type = 'European Parliament' THEN e1.prev_ep_eid " +
+                                "END) = e2.e_id " +
+                        "WHERE e1.type = e2.type AND e2.cab_year > e2.election_year " +
                             "AND e2.cab_year < e1.election_year";
 
-        List<Integer> elections;
-        List<Integer> cabinets;
-        ElectionCabinetResult result;
+        List<Integer> elections = new ArrayList<Integer>();
+        List<Integer> cabinets = new ArrayList<Integer>();
         
         try {
-            stmt = connection.createStatement();
+            Statement stmt = connection.createStatement();
             ResultSet res = stmt.executeQuery(query);
             
             while (res.next()) {
-                int election_id = res.getString("e_id");
+                int election_id = res.getInt("e_id");
                 elections.add(election_id);
-                int cabinet_id = res.getString("cab_id");
+                int cabinet_id = res.getInt("cab_id");
                 cabinets.add(cabinet_id);
             }
         } catch (SQLException e) {
@@ -87,25 +85,59 @@ public class Assignment2 extends JDBCSubmission {
             res.close();
             stmt.close();
         }
-        result = new ElectionCabinetResult(elections, cabinets);
-        return result;
+        return new ElectionCabinetResult(elections, cabinets);
     }
 
     @Override
     public List<Integer> findSimilarPoliticians(Integer politicianName, Float threshold) {
-        Statement stmt = null;
-        String query = "";
 
         List<Integer> result = new ArrayList<Integer>();
+        String description = new String("");
+        String comment = new String("");
+
+        String query1 = "SELECT description, comment " +
+                        "FROM politician_president " +
+                        "WHERE id = " + politicianName;
+        
+        String query2 = "SELECT id, description, comment " +
+                        "FROM politician_president " +
+                        "WHERE id <> " + politicianName;
 
         try {
-            stmt = connection.createStatement();
-            ResultSet res = stmt.executeQuery(query);
+            Statement stmt = connection.createStatement();
+            ResultSet res = stmt.executeQuery(query1);
+
+            // description and comment of specified president
+            while(res.next()) {
+                description = res.getString("description");
+                comment = res.getString("comment");
+            }
+
+            Statement stmt2 = connection.createStatement();
+            ResultSet res2 = stmt2.executeQuery(query2);
+
+            // descriptions and comments of all the other presidents
+            while(res2.next()) {
+                String otherDescription = res2.getString("description");
+                String otherComment = res2.getString("comment");
+
+                double similarity = similarity(description + " " + comment, 
+                                                otherDescription + " " + otherComment);
+                double similarity2 = similarity(comment + " " + description,
+                                                 otherComment + " " + otherDescription);
+
+                if (similarity >= threshold || similarity2 >= threshold) {
+                    int id = res2.getInt("id");
+                    result.add(id);
+                }
+            }
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         } finally {
             res.close();
             stmt.close();
+            res2.close();
+            stmt2.close();
         }
         return result;
     }
